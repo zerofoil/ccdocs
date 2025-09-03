e9d09545dd27bcbe6c83ca509553400cf8c92e4a
Zero <zerofoil523934@gmail.com>
2025-09-03 18:47:11 +0200

    update

--- replacer.py
+++ replacer.py
@@ -8,16 +8,19 @@ import os
 total = 0
 
 for path, _, files in os.walk("docs"):
-    if path.contains("/.") continue
+    if "/." in path: continue
     for file in files:
+        if ".md" not in file: continue
         filepath = os.path.join(path, file)
         with open(filepath, "r", encoding="utf-8") as this:
-            text = this.read()
+            try:
+                text = this.read()
+            except Exception:
+                print(f"Error at: {filepath}")
         if find in text:
             text = text.replace(find, replace)
             with open(filepath, "w", encoding="utf-8") as this:
                 this.write(text)
-            
             total += 1
 
 print(f"Successfully replaced text in {total} files.")
